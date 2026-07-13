8e5865bdbca3f0b162d18357f218811e0c785627
Jipeng Zhang <relaxfinger@gmail.com>
2026-07-13 19:09:49 +0700

    Remove obsolete UIKit router helper

--- URLRouter/UIViewController+TopMostViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UIViewController+TopMostViewController.swift
-//  URLRouter
-//
-//  Copyright (c) 2026 relaxfinger
-//  SPDX-License-Identifier: MIT
-//
-
-// UIKit global window discovery was removed. Each SwiftUI scene owns an AppRouter.
